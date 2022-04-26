9967ca8999cccd803a2ca4e32382f79c679ef0cd
TW-Chauge chen <49526083+Chaugechen@users.noreply.github.com>
2022-04-26 19:46:56 +0800

    Add files via upload

---
+++ DataGridViewscroll/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewscroll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i =0 ;i< 100;i++)
+            {
+                dataGridView1.Rows.Add(i,"User" + i);
+            }
+            new TouchGrid(dataGridView1);        
+        }
+    }
+}
