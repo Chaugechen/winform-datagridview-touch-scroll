2253f3900968ea737b2f984d6d9d37e4c5acd6ab
TW-Chauge chen <49526083+Chaugechen@users.noreply.github.com>
2022-04-26 19:45:52 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DataGridViewscroll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UserName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.測試1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.測試2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.測試3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.UserName,
+            this.測試1,
+            this.測試2,
+            this.測試3});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(320, 393);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // UserName
+            // 
+            this.UserName.HeaderText = "Name";
+            this.UserName.Name = "UserName";
+            this.UserName.ReadOnly = true;
+            // 
+            // 測試1
+            // 
+            this.測試1.HeaderText = "測試1";
+            this.測試1.Name = "測試1";
+            this.測試1.ReadOnly = true;
+            // 
+            // 測試2
+            // 
+            this.測試2.HeaderText = "測試2";
+            this.測試2.Name = "測試2";
+            this.測試2.ReadOnly = true;
+            // 
+            // 測試3
+            // 
+            this.測試3.HeaderText = "測試3";
+            this.測試3.Name = "測試3";
+            this.測試3.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 393);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 測試1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 測試2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 測試3;
+    }
+}
+
---
+++ TouchGrid.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewscroll
+{
+    public class TouchGrid
+    {
+        private readonly DataGridView _View;
+        int startDragRowhandle = -1;
+        int startDragColumnhandle = -1;
+        int topRowIndex = -1;
+        int topColumnIndex = -1;
+        private bool _IsDragging; 
+
+        public bool IsDragging
+        {
+            get { return _IsDragging; }
+            set { _IsDragging = value; }
+        }
+        public TouchGrid(DataGridView view)
+        {
+            _View = view;
+            InitViewPropertins();
+        }
+        private void InitViewPropertins() 
+        {
+            _View.Cursor = Cursors.Hand;
+            _View.MultiSelect = false;
+            _View.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            _View.MouseDown += _View_MouseDown;
+            _View.MouseMove += _View_MouseMove;
+            _View.MouseUp += _View_MouseUp;
+            _View.Layout += new LayoutEventHandler(_View_Layout);
+            _View.DataError += new DataGridViewDataErrorEventHandler(_View_DataError);
+        }
+        private void _View_DataError(object sender,DataGridViewDataErrorEventArgs e)
+        {          
+        }
+
+        DataGridView.HitTestInfo myHitTest;
+        private int GetRowUnderCursor(Point location)
+        {
+            myHitTest = _View.HitTest(location.X,location.Y);
+            return myHitTest.RowIndex;
+
+        }
+
+        DataGridView.HitTestInfo myHitTest2;
+        private int GetColumnUnderCursor(Point location)
+        {
+            myHitTest2 = _View.HitTest(location.X, location.Y);
+            return myHitTest2.ColumnIndex;
+        }
+
+
+        void _View_Layout(object sender,LayoutEventArgs e)
+        {
+            try {
+                IsDragging = false;            
+            }
+            catch
+            {
+
+            }
+        }
+        private void DoScrollX(int delta)
+        {
+            if (delta == 0)
+                return;
+            if (delta < 0 && _View.FirstDisplayedScrollingRowIndex == 0)
+                delta = 0;
+            _View.FirstDisplayedScrollingRowIndex += delta;
+
+        }
+        private void DoScrollY(int delta)
+        {
+            if (delta == 0)
+                return;
+            if (delta < 0 && _View.FirstDisplayedScrollingColumnIndex == 0)
+                delta = 0;
+            _View.FirstDisplayedScrollingColumnIndex += delta;
+
+        }
+        private void _View_MouseUp(object sender,MouseEventArgs e)
+        {
+            try
+            {
+                IsDragging = false;
+            }
+            catch
+            {}
+         
+        }
+        private void _View_MouseMove(object sender,MouseEventArgs e)
+        {
+            try
+            {
+                if (IsDragging)
+                {
+                    int newRowX = GetRowUnderCursor(e.Location);
+                    if (newRowX < 0)
+                        return;
+                    int deltaX = startDragRowhandle - newRowX;
+                    DoScrollX(deltaX);
+
+                    int newRowY = GetColumnUnderCursor(e.Location);
+                    if (newRowY < 0)
+                        return;
+                    int deltaY = startDragColumnhandle - newRowY;
+                    DoScrollY(deltaY);
+                }
+            }
+            catch{ }
+        }
+        private void _View_MouseDown(object sender, MouseEventArgs e)
+        {
+            try 
+            {
+                IsDragging = true;
+                startDragRowhandle = GetRowUnderCursor(e.Location);
+                topRowIndex = _View.FirstDisplayedScrollingRowIndex;
+
+                startDragColumnhandle = GetColumnUnderCursor(e.Location);
+                topColumnIndex = _View.FirstDisplayedScrollingColumnIndex;
+
+            }
+            catch { }
+        }
+    }
+
+
+}
